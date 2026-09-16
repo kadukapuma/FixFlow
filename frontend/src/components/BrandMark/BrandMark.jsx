615dddcd9647447b69b2df7988d54282b5f3cb69
kadukapuma <kadukapuma@gmail.com>
2026-09-16 12:41:33 +0530

    change logo

--- frontend/src/components/BrandMark/BrandMark.jsx
+++ frontend/src/components/BrandMark/BrandMark.jsx
@@ -1,11 +1,10 @@
+import fixflowMark from "../../assets/fixflow_mark.png";
 import "./BrandMark.css";
 
 function BrandMark({ size = 40 }) {
     return (
         <div className="brand-mark" style={{ width: size, height: size }}>
-            <svg viewBox="0 0 24 24" width={size * 0.46} height={size * 0.46} fill="none">
-                <path d="M12 5v14M5 12h14" stroke="#ddfb5e" strokeWidth="2.4" strokeLinecap="round" />
-            </svg>
+            <img src={fixflowMark} alt="FixFlow" className="brand-mark__img" />
         </div>
     );
 }
